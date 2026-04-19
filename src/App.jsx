ef4a034c618c213ba0f8f9f21c4c7be4bbdcd005
aarushibamal-cmyk <aarushibamal@gmail.com>
2026-04-19 19:41:39 +0530

    updated changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
+import { Analytics } from "@vercel/analytics/next"
 
 const App = () => {
   const [userData,setUserData] = useState([]);
